2f454a62a78336c777499e51d5d891fc956394db
OhadR-Cellebrite-PC <ohad.redlich@cellebrite.com>
2019-05-24 14:52:02 +0300

    add a Counter

--- src/main/java/com/ohadr/metrics/SomeService.java
+++ src/main/java/com/ohadr/metrics/SomeService.java
@@ -2,20 +2,35 @@ package com.ohadr.metrics;
 
 import java.util.Set;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
+
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
+import com.codahale.metrics.Counter;
+import com.codahale.metrics.MetricRegistry;
 
 @Component
 public class SomeService
 {
 	private static Logger log = Logger.getLogger(SomeService.class);
 	
+    private Counter eventCounterMetric;
+
+    @Resource
+    MetricRegistry metrics;
 
 	public SomeService()
 	{
 	}
+
+    @PostConstruct
+    public void init() {
+        eventCounterMetric = new Counter();
+    }
 
 	public void send( final String message )
 	{
@@ -23,7 +38,11 @@ public class SomeService
 	
 	public void send(String message, int amount) {
 		for(int i = 0; i < amount; ++i)
+		{
 			send(message + "#" + i);
+			eventCounterMetric.inc();
+		}
+		
 	}
 
 }
